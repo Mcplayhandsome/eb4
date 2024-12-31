886aeb8f2f36967f061f7cb0f25cbd05507565d1
bonnie chung <mcplayhandsome@gmail.com>
2024-12-31 16:50:35 +0800

    step 11 contacts account dashboard

--- bcre/settings.py
+++ bcre/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig'
 ]
 
 MIDDLEWARE = [
@@ -150,3 +151,9 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     messages.SUCCESS: 'success',
 }
+
+#EMAIL_HOST = 'smtp.gmail.com',
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'mcplayhandsome@gmail.com'
+#EMAIL_HOST_PASSWORD = 'mcplay2025'
+#EMAIL_USE_TLS = True
--- bcre/static/js/main.js
+++ bcre/static/js/main.js
@@ -1,2 +1,7 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+$(document).ready(function(){
+    setTimeout(()=>{
+        $("#message").fadeOut("slow");
+    },3000);
+});
